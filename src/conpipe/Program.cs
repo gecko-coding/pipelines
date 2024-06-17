2d6299cb5f9b6fdb868a189b882c8c6cbd5f10ce
Dylan Miles <dylan.g.miles@gmail.com>
2024-06-17 16:08:48 +0200

    Conpipe and detect if redirected stdin Trying to chain console apps in terminals

--- src/conpipe/Program.cs
+++ src/conpipe/Program.cs
@@ -1,16 +1,39 @@
 ﻿using System.Buffers;
 using System.Collections.Concurrent;
+using System.ComponentModel.Design;
 using System.IO.Pipelines;
-using System.Text;
 
-namespace capitalize;
+namespace conpipe;
 
+/// <summary>
+/// The main function of this program is to take input from std in and pipe out as it is ready.
+/// </summary>
 class Program
 {
     static async Task Main(string[] args)
     {
-        var stdout = Console.OpenStandardOutput();
-        await ProcessInputAsync(stdout);
+        Stream destination = null;
+        if (args.Length > 0)
+        {
+            Console.WriteLine($"Opening {args[0]}");
+            destination = File.Open(args[0], FileMode.Append);
+        }
+        else
+        {
+            Console.WriteLine($"Opening stdout");
+            destination = Console.OpenStandardError();
+        }
+        
+        await ProcessInputAsync(destination!);
+    }
+    static async Task ProcessData(ReadOnlySequence<byte> chunk, Stream destination)
+    {
+        var segment = chunk.Start;
+        while (chunk.TryGet(ref segment, out var readOnlyMemory, advance: true))
+        {
+            await destination.WriteAsync(readOnlyMemory);
+            await destination.FlushAsync();
+        }
     }
 
     static async Task ProcessInputAsync(Stream destination)
@@ -31,14 +54,12 @@ class Program
         // Input is from a console
         if (Console.IsInputRedirected == false)
         {
-            Console.WriteLine("Read from console");
             writing = FillPipeFromConsoleAsync(pipe.Writer);    
         }
         
         // Input is from a file
         if (Console.IsInputRedirected == true)
         {
-            Console.WriteLine("Read from redirected stdin");
             var source = Console.OpenStandardInput();
             writing = FillPipeAsync(source, pipe.Writer);    
         }
@@ -46,34 +67,6 @@ class Program
         Task reading = ReadPipeAsync(pipe.Reader, destination);
 
         await Task.WhenAll(reading, writing!);
-    }
-    
-    static async Task ProcessLine(ReadOnlySequence<byte> line, Stream destination)
-    {
-        var segment = line.Start;
-        while (line.TryGet(ref segment, out var readOnlyMemory, advance: true))
-        {
-            await destination.WriteAsync(readOnlyMemory);
-        }
-    }
-
-    static async Task ProcessLinesAsync(Stream source, Stream destination)
-    {
-        // Create schedulers
-        var writeScheduler = new SingleThreadPipeScheduler();
-        var readScheduler = new SingleThreadPipeScheduler();
-        
-        // The Pipe will start returning incomplete tasks from FlushAsync until
-        // the reader examines at least 5 bytes.
-        var options = new PipeOptions(pauseWriterThreshold: 10, resumeWriterThreshold: 5,readerScheduler: readScheduler,
-            writerScheduler: writeScheduler,
-            useSynchronizationContext: false);
-        var pipe = new Pipe(options);
-        
-        Task writing = FillPipeFromConsoleAsync(pipe.Writer);
-        Task reading = ReadPipeAsync(pipe.Reader, destination);
-
-        await Task.WhenAll(reading, writing);
     }
 
     static async Task FillPipeFromConsoleAsync(PipeWriter writer)
@@ -88,21 +81,34 @@ class Program
                     var key = Console.ReadKey(false);
                     
                     var c = key.KeyChar;
+                    
+                    memory.Span[0] = (byte)c;
+                    
                     if (char.IsControl(c))
                     {
                         c = (char)key.Key;
-                        
+                    
                         // Handle special characters ...
                         if (key.Key == ConsoleKey.Backspace)
                         {
                             // Simulate backspace
                             Console.SetCursorPosition(Console.GetCursorPosition().Left - 1, Console.GetCursorPosition().Top);
                             Console.Write("\b \b");
+                            
+                            memory.Span[0] = (byte)'\b';
                         }
-                    }                   
-                    
-                    memory.Span[0] = (byte)c;
-                    
+                        
+                        // Handle special characters ...
+                        if (key.Key == ConsoleKey.Enter)
+                        {
+                            // Simulate backspace
+                            //Console.SetCursorPosition(Console.GetCursorPosition().Left - 1, Console.GetCursorPosition().Top);
+                            Console.Write("\n");
+                            
+                            memory.Span[0] = (byte)'\n';
+                        }
+                    }
+
                     // Tell the PipeWriter how much was read from the Socket.
                     writer.Advance(1);
                 }
@@ -130,7 +136,7 @@ class Program
 
     static async Task FillPipeAsync(Stream source, PipeWriter writer)
     {
-        const int minimumBufferSize = 5;
+        const int minimumBufferSize = 512;
         
         while (true)
         {
@@ -173,10 +179,10 @@ class Program
             ReadResult result = await reader.ReadAsync();
             ReadOnlySequence<byte> buffer = result.Buffer;
 
-            while (TryReadWord(ref buffer, out ReadOnlySequence<byte> line))
+            while (TryReadChunk(ref buffer, out ReadOnlySequence<byte> chunk))
             {
                 // Process the line.
-                await ProcessLine(line, destination);
+                await ProcessData(chunk, destination);
             }
 
             // Tell the PipeReader how much of the buffer has been consumed.
@@ -193,26 +199,20 @@ class Program
         await reader.CompleteAsync();
     }
 
-    static bool TryReadWord(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> word)
+    static bool TryReadChunk(ref ReadOnlySequence<byte> buffer, out ReadOnlySequence<byte> chunk)
     {
-        // Look for a space or EOL in the buffer.
-        SequencePosition? position = buffer.PositionOf((byte)' ');
-        if (position == null)
+        // Check if no data
+        if (buffer.Length == 0)
         {
-            position = buffer.PositionOf((byte)'\n');
-        }
-
-        if (position == null)
-        {
-            word = default;
+            chunk = default;
             return false;
         }
         
-        // Include the ' ' or \n
-        position = buffer.GetPosition(1, position.Value);
-
+        // Look for a space or EOL in the buffer.
+        SequencePosition? position = buffer.GetPosition(buffer.Length);
+        
         // Capture the word
-        word = buffer.Slice(0, position.Value);
+        chunk = buffer.Slice(0, position.Value);
         
         // Move the buffer along
         buffer = buffer.Slice(position.Value);
